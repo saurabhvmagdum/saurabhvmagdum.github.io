43a7cfb30972df12787799c1156c141b8c7eb988
saurabhvmagdum@gmail.com <saurabhvmagdum@gmail.com>
2022-10-29 09:45:12 +0530

    adding portfolio files

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-       if(this.scrollY >20){
-           $('.navbar').addClass("sticky");
-   
-       }else{
-           $(`.navbar`).removeClass("sticky")
-   
-       }
+const ham = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+ham.addEventListener("click",() => {
+    ham.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>{
+    n.addEventListener("click",() => {
+        ham.classList.remove("active");
+        navMenu.classList.remove("active");
     })
-    $('.menu-btn').click(function(){
-       $(`.navbar .menu`).toggleClass("active");
-       $(`.menu-btn i`).toggleClass("active")
+})
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        // console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
     })
-   })
+})
+
+const hiddenElement = document.querySelectorAll(".hidden");
+hiddenElement.forEach((element)=> observer.observe(element));
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "saurabhvmagdum", { responsive: true });
+
+  
