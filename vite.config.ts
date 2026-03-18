be73ce08676abf7a7cb2b0073543035e2c41b58e
Saurabh Magdum <101392340+saurabhvmagdum@users.noreply.github.com>
2026-03-18 12:56:57 +0530

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite_react_shadcn_ts/',
   plugins: [react()],
-  base: '/'
 });
