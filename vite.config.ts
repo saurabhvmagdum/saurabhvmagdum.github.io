7cc8c0330ee2fb77fb11dab479772e26dd7d5c55
Saurabh Magdum <101392340+saurabhvmagdum@users.noreply.github.com>
2026-03-18 12:55:02 +0530

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/vite_react_shadcn_ts/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  build: {
-    minify: "esbuild",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/'
+});
